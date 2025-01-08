66a583bc9cd03bb6600fd4917065f936b48f418e
Natalia.Mangili <nataliaperico89@gmail.com>
2025-01-08 20:56:47 -0300

    feat: Helpers Channel

---
+++ shared/Helpers/Channels/ChannelHelperExtension.cs
@@ -0,0 +1,41 @@
+﻿using Grpc.Net.Client;
+using Microsoft.Extensions.Configuration;
+
+namespace Helpers.Channels;
+
+public static class ChannelHelperExtension
+{
+    public static GrpcChannel CreateGrpcChannel(IConfiguration config, string serviceName)
+    {
+        string channelUrl = GetServiceUrl(config, serviceName);
+        if (string.IsNullOrEmpty(channelUrl))
+        {
+            throw new InvalidOperationException($"Service URL for '{serviceName}' is not valid/configured.");
+        }
+
+        return GrpcChannel.ForAddress(channelUrl, CreateGrpcChannelOptions());
+    }
+
+    private static string GetServiceUrl(IConfiguration config, string serviceName)
+    {
+        string? url = config[$"services:{serviceName}Url"];
+        if (string.IsNullOrEmpty(url))
+        {
+            throw new InvalidOperationException($"Configuration for '{serviceName}Url' is missing or empty.");
+        }
+        return url;
+    }
+
+    private static GrpcChannelOptions CreateGrpcChannelOptions()
+    {
+        return new GrpcChannelOptions { HttpHandler = CreateHttpClientHandler() };
+    }
+
+    private static HttpClientHandler CreateHttpClientHandler()
+    {
+        return new HttpClientHandler
+        {
+            ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+        };
+    }
+}
