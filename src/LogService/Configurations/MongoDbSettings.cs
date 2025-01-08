9d82cc7b421c2de5a8d89f291591ffc45d3d37d8
Natalia.Mangili <nataliaperico89@gmail.com>
2025-01-08 20:55:51 -0300

    feat: Preparando para implementação MongoDB em Log

---
+++ src/LogService/Configurations/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace LogService.Configurations;
+
+public class MongoDbSettings
+{
+    public string ConnectionString { get; set; } = null!;
+    public string DatabaseName { get; set; } = null!;
+    public string LogsCollectionName { get; set; } = "Logs";
+}
