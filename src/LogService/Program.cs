9db51e2c42d0721349010a6cd5fa9dd961aefa9c
Natalia.Mangili <nataliaperico89@gmail.com>
2025-01-09 20:45:16 -0300

    feat: Implementação do gRPC/proto no Service de Log

--- src/LogService/Program.cs
+++ src/LogService/Program.cs
@@ -1,32 +1,20 @@
+using LogService.Configurations;
+using LogService.Repositories;
+using LogService.Services;
+//using LogService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//MongoDB
+builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection("MongoDbSettings"));
+
+builder.Services.AddSingleton<ILogRepository, LogRepository>();
+builder.Services.AddGrpc();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.MapGrpcService<LogGrpcService>();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
+app.MapGet("/", () => "Use gRPC");
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ src/LogService/Services/LogGrpcService.cs
@@ -0,0 +1,46 @@
+﻿using Grpc.Core;
+using LogService.Models;
+using LogService.Repositories;
+using Helpers.Protos.LogService;
+
+namespace LogService.Services;
+
+public class LogGrpcService(ILogRepository logRepository) : LogServiceApi.LogServiceApiBase
+{
+    private readonly ILogRepository _logRepository = logRepository;
+
+    public override async Task<LogResponse> SaveLog(LogRequest request, ServerCallContext context)
+    {
+        LogEntry logEntry = new()
+        {
+            Level = request.Level,
+            Message = request.Message,
+            Source = request.Source,
+            Details = request.Details
+        };
+
+        await _logRepository.SaveLogAsync(logEntry);
+
+        return new LogResponse { Message = "Log salvo com sucesso" };
+    }
+
+    public override async Task GetLogs(LogFilter request, IServerStreamWriter<LogMessage> responseStream, ServerCallContext context)
+    {
+        IEnumerable<LogEntry> logs = await _logRepository.GetLogsAsync(request.Source, request.Level);
+
+        logs.ToList().ForEach(async log =>
+        {
+            LogMessage logMessage = new()
+            {
+                Id = log.Id,
+                Timestamp = log.Timestamp.ToString("o"),
+                Level = log.Level,
+                Message = log.Message,
+                Source = log.Source,
+                Details = log.Details
+            };
+
+            await responseStream.WriteAsync(logMessage);
+        });
+    }
+}
