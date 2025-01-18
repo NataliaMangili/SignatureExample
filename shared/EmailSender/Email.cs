622115748cee534431303c62c688388cc902f991
Natalia.Mangili <nataliaperico89@gmail.com>
2025-01-18 18:03:31 -0300

    wip: EmailSender

---
+++ shared/EmailSender/Email.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Mail;
+
+namespace EmailSender;
+
+public class Email
+{
+    public async Task SendEmail(Dictionary<string, string> properties)
+    {
+        var studentEmail = properties["email"];
+        var smtpClient = new SmtpClient("smtp.example.com")
+        {
+            Port = 587,
+            Credentials = new System.Net.NetworkCredential("email@email.com", "password"),
+            EnableSsl = true,
+        };
+
+        var mailMessage = new MailMessage
+        {
+            From = new MailAddress("email@email.com"),
+            Subject = "Welcome!",
+            Body = "Test",
+            IsBodyHtml = true,
+        };
+        mailMessage.To.Add(studentEmail);
+
+        smtpClient.Send(mailMessage);
+    }
+}
