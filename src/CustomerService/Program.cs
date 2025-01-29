9ad67fe656eea9e0e8c72a371625fe3fd2cf2985
Natalia.Mangili <nataliaperico89@gmail.com>
2025-01-29 20:54:11 -0300

    cleanup: Program

--- src/CustomerService/Program.cs
+++ src/CustomerService/Program.cs
@@ -41,7 +41,3 @@ host.Run();
 
 app.Run();
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
