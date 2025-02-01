d92985b3f5053cb1fc524ad12e9120d80de08ff1
Natalia.Mangili <nataliaperico89@gmail.com>
2025-02-01 17:40:41 -0300

    feat: Criando Eventos de Pagamento

---
+++ shared/Helpers/Events/PaymentFailedEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace PaymentService.Events;
+
+public class PaymentFailedEvent
+{
+    public string TransactionId { get; set; }
+    public string Reason { get; set; }
+}
---
+++ shared/Helpers/Events/PaymentProcessedEvent.cs
@@ -0,0 +1,12 @@
+﻿using EventBus;
+
+namespace Helpers.Events;
+
+public class PaymentProcessedEvent : IntegrationEvent
+{
+    public string CustomerId { get; set; }  // ID do cliente
+    public string DeliveryId { get; set; }  // ID do pedido de entrega
+    public decimal Amount { get; set; }     // Valor pago
+    public string TransactionId { get; set; }  // ID da transação pagamento
+    public DateTime PaymentDate { get; set; }
+}
