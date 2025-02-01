d387e7625c107e350b7df7c6ac2c5e512cf59860
Natalia.Mangili <nataliaperico89@gmail.com>
2025-02-01 17:56:30 -0300

    feat: DeliveryConfirmedEvent

---
+++ shared/Helpers/Events/DeliveryConfirmedEvent.cs
@@ -0,0 +1,10 @@
+﻿using EventBus;
+
+namespace Helpers.Events;
+
+public class DeliveryConfirmedEvent : IntegrationEvent
+{
+    public string CustomerId { get; set; }  // ID do cliente
+    public string DeliveryId { get; set; }  // ID do pedido de entrega
+    public decimal Amount { get; set; }     // Valor do pagamento
+}
