d7ad34ec92d72ebbeed3e17aaee3ad41b1810f23
Natalia.Mangili <nataliaperico89@gmail.com>
2025-02-03 20:28:16 -0300

    feat: Scheduler Pagamento

---
+++ src/PaymentService/Services/EnrollCustomer.cs
@@ -0,0 +1,16 @@
+﻿using Helpers.Dtos;
+
+namespace PaymentService.Services;
+
+//Chama a tarefa em segundo pano
+public class EnrollCustomer(Scheduler taskScheduler, ILogger<EnrollCustomer> logger)
+{
+    private readonly Scheduler _taskScheduler = taskScheduler;
+    private readonly ILogger<EnrollCustomer> _logger = logger;
+
+    public void Invoke(EnrollCustomerInputData data)
+    {
+        _logger.LogInformation($"Scheduling payment for customer {data.CustomerId}");
+        _taskScheduler.Schedule("process_payment", data);
+    }
+}
---
+++ src/PaymentService/Services/Scheduler.cs
@@ -0,0 +1,13 @@
+﻿namespace PaymentService.Services;
+
+//Agendamento de Tarefas em segundo plano
+public class Scheduler(ILogger<Scheduler> logger)
+{
+    private readonly ILogger<Scheduler> _logger = logger;
+
+    public void Schedule(string taskName, object data)
+    {
+        //Logica da tarefa em segundo plano
+        _logger.LogInformation($"Task {taskName} scheduled with data: {data}");
+    }
+}
