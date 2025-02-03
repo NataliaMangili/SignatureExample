9af30526ac4af098384e23c09d95c5051daaf72a
Natalia.Mangili <nataliaperico89@gmail.com>
2025-02-03 20:31:50 -0300

    feat: PaymentRepository

---
+++ src/PaymentService/Data/PaymentRepository.cs
@@ -0,0 +1,13 @@
+﻿using PaymentService.Models;
+
+namespace PaymentService.Data;
+
+//Logica de acesso ao banco para salvar as classes e get delas
+public class PaymentRepository
+{
+    private readonly List<Payment> _payments = [];
+
+    public void Save(Payment payment) => _payments.Add(payment);
+
+    public List<Payment> GetAll() => _payments;
+}
