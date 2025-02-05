857c7e6cb114c98d267d4ccceac01a1135b553ba
Natalia.Mangili <nataliaperico89@gmail.com>
2025-02-04 21:21:13 -0300

    feat: PaymentService Models

---
+++ src/PaymentService/Models/Transaction.cs
@@ -0,0 +1,8 @@
+﻿namespace PaymentService.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public string PaymentId { get; set; }
+    public DateTime Timestamp { get; set; }
+}
