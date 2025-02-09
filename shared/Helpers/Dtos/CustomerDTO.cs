cf07274c0927550ea16097d30a91e4e0ffa9fa9f
Natalia.Mangili <nataliaperico89@gmail.com>
2025-02-09 12:06:38 -0300

    feat: Protos e DTOS

---
+++ shared/Helpers/Dtos/CustomerDTO.cs
@@ -0,0 +1,30 @@
+﻿using DeliveryService.Models;
+
+namespace Helpers.Dtos;
+
+public class CustomerDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+
+    public CustomerDTO() { }
+
+    public CustomerDTO(Guid id, string name, string email, string password)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Password = password;
+    }
+
+    public static CustomerDTO FromEntity(Customer customer)
+    {
+        return new CustomerDTO
+        {
+            Name = customer.Name,
+            Email = customer.Email
+        };
+    }
+}
