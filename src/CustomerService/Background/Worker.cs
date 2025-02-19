25375ac40dd9b12f45d5ef14f2cc7158eb8c31bb
Natalia.Mangili <nataliaperico89@gmail.com>
2025-02-19 19:17:04 -0300

    feat: CustomerService Worker e RabbitMq credenciais

--- src/CustomerService/Background/Worker.cs
+++ src/CustomerService/Background/Worker.cs
@@ -1,20 +1,34 @@
-﻿using EventBus.Abstractions;
-using CustomerService.Events;
+﻿using CustomerService.Events;
+using EventBus.Abstractions;
 using Helpers.Events;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace CustomerService.Background;
 
-public class Worker(ILogger<Worker> logger, IEventBus eventBus) : BackgroundService
+public class Worker : BackgroundService
 {
-    private readonly ILogger<Worker> _logger = logger;
-    private readonly IEventBus _eventBus = eventBus;
+    private readonly ILogger<Worker> _logger;
+    private readonly IEventBus _eventBus;
+
+    public Worker(ILogger<Worker> logger, IEventBus eventBus)
+    {
+        _logger = logger;
+        _eventBus = eventBus;
+    }
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        // Assinar eventos
+        // Inscrever-se para o evento
         _eventBus.Subscribe<CustomerEnrolledEvent, CustomerEnrolledEventHandler>();
 
-        _logger.LogInformation("Worker listening events...");
+        _logger.LogInformation("Worker listening to events...");
+
+        // O método ExecuteAsync continuará rodando enquanto o serviço não for interrompido
+        // O token de cancelamento é verificado durante a execução
+
+        stoppingToken.ThrowIfCancellationRequested(); // Permite interromper o serviço caso seja solicitado
+
         return Task.CompletedTask;
     }
 }
