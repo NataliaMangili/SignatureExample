f41a50f03eb44c1b26d68d82ca6f11ce88ea1dfb
Natalia.Mangili <nataliaperico89@gmail.com>
2025-02-22 09:07:26 -0300

    fix: launchsettings swagger e portas

---
+++ gateway/SignatureExampleAPI/class1.cs
@@ -0,0 +1,52 @@
+﻿//worker_processes 1;
+
+//events {
+//    worker_connections 1024;
+//}
+
+//http {
+//    include       mime.types;
+//    default_type  application/octet-stream;
+
+//    sendfile        on;
+//    keepalive_timeout 65;
+
+//     # Nome do container e porta
+
+//    upstream delivery_service {
+//        server deliveryservice:5001;  
+//    }
+
+//    upstream payment_service {
+//        server paymentservice:5002;  
+//    }
+
+//    upstream log_service {
+//        server logservice:5003; 
+//    }
+
+//    upstream email_service {
+//        server emailservice:5004; 
+//    }
+
+//    server {
+//        listen 80;
+
+//        # Rota para o Service
+//        location /api/delivery {
+//            proxy_pass http://delivery_service;
+//        }
+
+//        location /api/payment {
+//            proxy_pass http://payment_service;
+//        }
+
+//        location /api/logs {
+//            proxy_pass http://log_service;
+//        }
+
+//        location /api/email {
+//            proxy_pass http://email_service;
+//        }
+//    }
+//}
